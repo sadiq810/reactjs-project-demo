2280c27bc174191ca0dab0f0c41f03e4b6599f80
sadiq <you@example.com>
2021-07-07 12:38:02 +0500

    loan components are added

---
+++ src/components/apply_loan_components/YourRateComponent.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {connect} from "react-redux";
+import {Link, withRouter} from "react-router-dom";
+
+const YourRateComponent = ({user, history}) => {
+    return (
+        <div className="container-fluid mWidth95percent bg-white">
+            <div className="container pt-5 pb-5">
+                <div className="row program--wrapper">
+                    <div className="row">
+                        <Link to={'/loans?tab=draft'} className={'d-inline-block font-weight--bold float-end text-end'}>Residence Save & Exit</Link>
+                    </div>
+                    <div className="row mt-40">
+                        <div className="col-lg-3 col-md-3">
+                            <ul className={'loan--sub-menu'}>
+                                <li className={'position-relative'}>
+                                    <div className={'selected'}>
+                                        <i className="icofont-disc"></i>Loan Info
+                                    </div>
+                                </li>
+                                <li><i className="icofont-check-circled"></i>About you</li>
+                                <li><i className={'icofont-check-circled'}></i>Residence</li>
+                                <li>Education</li>
+                                <li>Employment</li>
+                                <li>Your rates</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-6 col-md-3">
+                            <h4>Select which loan would you like to apply.</h4>
+                        </div>
+                        <div className="col-lg-3 col-md-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+const mapStateToProps = (state) => ({
+    user: state.users.user,
+    isLoggedIn: state.users.isLoggedIn,
+    error: state.users.error
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(YourRateComponent));
--- src/pages/ApplyForLoan.jsx
+++ src/pages/ApplyForLoan.jsx
@@ -1,10 +1,14 @@
 import React, {useState} from "react";
-import DraftsComponent from "../components/loans_components/DraftsComponent";
 import LoanTypesComponent from "../components/apply_loan_components/LoanTypesComponent";
 import LoanInfoComponent from "../components/apply_loan_components/LoanInfoComponent";
+import AboutYouComponent from "../components/apply_loan_components/AboutYouComponent";
+import ResidenceComponent from "../components/apply_loan_components/ResidenceComponent";
+import EducationComponent from "../components/apply_loan_components/EducationComponent";
+import EmploymentComponent from "../components/apply_loan_components/EmploymentComponent";
+import YourRateComponent from "../components/apply_loan_components/YourRateComponent";
 
 const ApplyForLoan = () => {
-    let [tab, setTab] = useState('loan_info');
+    let [tab, setTab] = useState('residence');
 
     const getComponent = () => {
         switch (tab) {
@@ -12,8 +16,16 @@ const ApplyForLoan = () => {
                 return <LoanTypesComponent />;
             case 'loan_info':
                 return <LoanInfoComponent/>;
-            case 'Drafts':
-                return <DraftsComponent />
+            case 'about_you':
+                return <AboutYouComponent />
+            case 'residence':
+                return <ResidenceComponent />
+            case 'education':
+                return <EducationComponent />
+            case 'employment':
+                return <EmploymentComponent />
+            case 'your_rate':
+                return <YourRateComponent />
             default:
                 return 'Please select a tab.';
         }
